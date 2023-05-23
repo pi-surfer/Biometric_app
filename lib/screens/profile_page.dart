5deb2153be4308f80d6cd17013509209fb9eeb45
Beatrice Martin <beatrice.martin.1@studenti.unipd.it>
2023-05-23 20:42:42 +0200

    new widget added

--- lib/screens/profile_page.dart
+++ lib/screens/profile_page.dart
@@ -38,118 +38,120 @@ class _ProfilePageState extends State<ProfilePage> {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Container(
-                  padding: EdgeInsets.only(top:20),
-                  child: Stack(
-                    children:[ 
-                      Container(
-                        margin: EdgeInsets.only(top:80),
-                        width: 400,
-                        height: 475,
-                        //width: MediaQuery.of(context).size.width,
-                        //height: MediaQuery.of(context).size.width,
-                        decoration: BoxDecoration(
-                          color: Color.fromARGB(114, 255, 236, 66),
-                          borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(40),
-                            topRight: Radius.circular(40),
-                            bottomLeft:Radius.circular(40),
-                            bottomRight:Radius.circular(40) 
-                          )
+            Center(
+              child: Container(
+                    padding: EdgeInsets.only(top:20),
+                    child: Stack(
+                      children:[ 
+                        Container(
+                          margin: EdgeInsets.only(top:80),
+                          //width: 400,
+                          //height: 475,
+                          width: MediaQuery.of(context).size.width*0.8,
+                          height: MediaQuery.of(context).size.width*0.6,
+                          decoration: BoxDecoration(
+                            color: Color.fromARGB(114, 255, 236, 66),
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(40),
+                              topRight: Radius.circular(40),
+                              bottomLeft:Radius.circular(40),
+                              bottomRight:Radius.circular(40) 
+                            )
+                          ),
+                          child: Column(
+                            children: [    
+                              Container(
+                                padding: EdgeInsets.only(top: 80, left: 20, right: 20),
+                                child: Column(
+                                  children: [
+                                    TextField(
+                                      controller: names,
+                                      decoration: InputDecoration(
+                                        labelText: 'Names',
+                                        prefixIcon: Icon(Icons.person),
+                                        border: myInputBorder(),
+                                        enabledBorder: myInputBorder(),
+                                        focusedBorder: myFocusBorder()
+                                      ),
+                                    ),
+                                    SizedBox(height: 15,),
+                                    TextField(
+                                      controller: names,
+                                      decoration: InputDecoration(
+                                        labelText: 'Username',
+                                        prefixIcon: Icon(Icons.people),
+                                        border: myInputBorder(),
+                                        enabledBorder: myInputBorder(),
+                                        focusedBorder: myFocusBorder()
+                                      ),
+                                    ),
+                                    SizedBox(height: 15,),
+                                    TextField(
+                                      controller: names,
+                                      decoration: InputDecoration(
+                                        labelText: 'Email',
+                                        prefixIcon: Icon(Icons.email),
+                                        border: myInputBorder(),
+                                        enabledBorder: myInputBorder(),
+                                        focusedBorder: myFocusBorder()
+                                      ),
+                                    ),
+                                    SizedBox(height: 15,),
+                                    TextField(
+                                      controller: names,
+                                      decoration: InputDecoration(
+                                        labelText: 'Password',
+                                        prefixIcon: Icon(Icons.password),
+                                        border: myInputBorder(),
+                                        enabledBorder: myInputBorder(),
+                                        focusedBorder: myFocusBorder()
+                                      ),
+                                    ),
+                                    SizedBox(height: 15),
+                                    ElevatedButton(onPressed: () {}, 
+                                    child: Text('Update'),
+                                    style: ElevatedButton.styleFrom(
+                                      primary: Color.fromARGB(255, 130, 204, 34),
+                                      padding: EdgeInsets.symmetric(
+                                        horizontal: 25, vertical: 20),
+                                        textStyle: TextStyle(
+                                          fontSize: 25, fontWeight: FontWeight.bold)
+                                    ),
+                                    ),
+                                    
+                              
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
                         ),
-                        child: Column(
-                          children: [    
-                            Container(
-                              padding: EdgeInsets.only(top: 80, left: 20, right: 20),
-                              child: Column(
-                                children: [
-                                  TextField(
-                                    controller: names,
-                                    decoration: InputDecoration(
-                                      labelText: 'Names',
-                                      prefixIcon: Icon(Icons.person),
-                                      border: myInputBorder(),
-                                      enabledBorder: myInputBorder(),
-                                      focusedBorder: myFocusBorder()
-                                    ),
-                                  ),
-                                  SizedBox(height: 15,),
-                                  TextField(
-                                    controller: names,
-                                    decoration: InputDecoration(
-                                      labelText: 'Username',
-                                      prefixIcon: Icon(Icons.people),
-                                      border: myInputBorder(),
-                                      enabledBorder: myInputBorder(),
-                                      focusedBorder: myFocusBorder()
-                                    ),
-                                  ),
-                                  SizedBox(height: 15,),
-                                  TextField(
-                                    controller: names,
-                                    decoration: InputDecoration(
-                                      labelText: 'Email',
-                                      prefixIcon: Icon(Icons.email),
-                                      border: myInputBorder(),
-                                      enabledBorder: myInputBorder(),
-                                      focusedBorder: myFocusBorder()
-                                    ),
-                                  ),
-                                  SizedBox(height: 15,),
-                                  TextField(
-                                    controller: names,
-                                    decoration: InputDecoration(
-                                      labelText: 'Password',
-                                      prefixIcon: Icon(Icons.password),
-                                      border: myInputBorder(),
-                                      enabledBorder: myInputBorder(),
-                                      focusedBorder: myFocusBorder()
-                                    ),
-                                  ),
-                                  SizedBox(height: 15),
-                                  ElevatedButton(onPressed: () {}, 
-                                  child: Text('Update'),
-                                  style: ElevatedButton.styleFrom(
-                                    primary: Color.fromARGB(255, 130, 204, 34),
-                                    padding: EdgeInsets.symmetric(
-                                      horizontal: 25, vertical: 20),
-                                      textStyle: TextStyle(
-                                        fontSize: 25, fontWeight: FontWeight.bold)
-                                  ),
-                                  ),
-                                  
-                            
-                                ],
+                        Align(
+                        alignment: Alignment.topCenter,
+                        child: Stack(
+                          children: [
+                            ClipOval(
+                              child: Image.asset('assets/images/avatar_Carciofo.jpg', width: 150, height: 150, fit: BoxFit.cover)
+                              ),
+                            Positioned(
+                              bottom: 5,
+                              right: 15.5,
+                              child: Container(
+                                padding: EdgeInsets.all(5),
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  shape: BoxShape.circle
+                                ),
+                                child: Icon(Icons.edit, size: 30),
                               ),
                             )
                           ],
                         ),
                       ),
-                      Align(
-                      alignment: Alignment.topCenter,
-                      child: Stack(
-                        children: [
-                          ClipOval(
-                            child: Image.asset('assets/images/avatar_Carciofo.jpg', width: 150, height: 150, fit: BoxFit.cover)
-                            ),
-                          Positioned(
-                            bottom: 5,
-                            right: 15.5,
-                            child: Container(
-                              padding: EdgeInsets.all(5),
-                              decoration: BoxDecoration(
-                                color: Colors.white,
-                                shape: BoxShape.circle
-                              ),
-                              child: Icon(Icons.edit, size: 30),
-                            ),
-                          )
-                        ],
-                      ),
+                      ]
                     ),
-                    ]
                   ),
-                ),
+            ),
 
                 // pensavo di rimandare alla pagina progetti o di mettere un expansion tile ma non riesco a farlo
                 SizedBox(height: 15),
