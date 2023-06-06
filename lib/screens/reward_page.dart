052d55531f6b30f853aa81b7c7b15befc48b1524
Lorenzo Sterzi <lorenzo.sterzi@studenti.unipd.it>
2023-06-06 09:11:57 +0200

    Scrolling Effect first steps

--- lib/screens/reward_page.dart
+++ lib/screens/reward_page.dart
@@ -1,49 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
-import 'package:app_project/screens/login_page.dart';
+//import 'package:app_project/screens/login_page.dart';
 
+final assets = [
+    // lista path assets
+];
 
-class RewardPage extends StatelessWidget {
+class RewardPage extends StatefulWidget {
   const RewardPage({Key? key}) : super(key: key);
 
-  static const routename = 'RewardPage';
+  @override
+  State createState() => _RewardPageState();
+}
 
+class _RewardPageState extends State<RewardPage> with TickerProviderStateMixin {
+  
   @override
   Widget build(BuildContext context) {
-    print('${RewardPage.routename} built');
     return Scaffold(
-      appBar: AppBar(
-        title: Text(RewardPage.routename),
-      ),
-      body: Center(
-        child: Text('login_flow'),
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.blueAccent,
-              ),
-              child: Text('login_flow'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.7,
+            child: PageView.builder(
+              controller: _pageController,
+              itemBuilder: ((context, index) {
+                return Container(
+                  margin: const EdgeInsets.symmetric(
+                    vertical: 32,
+                    horizontal: 16,
+                  ),
+                  color: Colors.red,
+                  //decoration: BoxDecoration(),
+                  child: Center(child: Text('Page $index'),),
+                );
+              }),
             ),
-            ListTile(
-              leading: Icon(Icons.logout),
-              title: Text('logout'),
-              onTap: () => _toLoginPage(context),
-            )
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
+}
 
-  void _toLoginPage(BuildContext context) {
-    // pop the drawer first
-    Navigator.pop(context);
-    // then pop the home page
-    Navigator.of(context)
-        .push(MaterialPageRoute(builder: (context) => LoginPage()));
+
+
+class _VideoScreenState extends State<VideosScreen> {
+  late PageController _pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(viewportFraction: 0.8);
   }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+}
+
+
+
+class VideoCard extends StatelessWidget {
+
+  final String assetPath;
+
+  const VideoCard({
+    super.key,
+    required this.assetPath,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Card();
+  }
+
 }
