890833f0b6dd717cf76ae6159ac35c3bd8d550d8
Lorenzo Sterzi <lorenzo.sterzi@studenti.unipd.it>
2023-06-15 18:19:53 +0200

    RewardPage layout rearranged

--- lib/screens/reward_page.dart
+++ lib/screens/reward_page.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 
 //import 'package:app_project/screens/login_page.dart';
-
-final assets = [
-    // lista path assets
-];
 
 class RewardPage extends StatefulWidget {
   const RewardPage({Key? key}) : super(key: key);
@@ -15,45 +10,104 @@ class RewardPage extends StatefulWidget {
 }
 
 class _RewardPageState extends State<RewardPage> with TickerProviderStateMixin {
+  final _pageController = PageController(viewportFraction: 0.8);
 
- final _pageController = PageController(viewportFraction: 0.8);
-  
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.all(10.0),
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 254, 251, 228),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding:
+              const EdgeInsets.only(left: 10, right: 10, top: 130, bottom: 400),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
+              SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.05,
+                  width: MediaQuery.of(context).size.width * 0.85,
+                  child: Column(
+                    children: [
+                      Text(
+                        'Hard work pays off!!',
+                        style: TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  )),
+              const SizedBox(
+                height: 20,
+              
+              ),
               Container(
-                height: MediaQuery.of(context).size.height * 0.4,
-                width: MediaQuery.of(context).size.width * 0.8,
-                      color: Colors.green),
+                height: MediaQuery.of(context).size.height * 0.1,
+                width: MediaQuery.of(context).size.width * 0.85,
+                child: ListView.builder(
+                  itemCount: 100,
+                  itemBuilder: (context, int index) {
+                    return Container(
+                      height: MediaQuery.of(context).size.height * 0.18,
+                      width: MediaQuery.of(context).size.width * 0.18,
+                      margin: const EdgeInsets.all(6.0),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        shape: BoxShape.circle,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.3),
+                            offset: const Offset(0, 2),
+                            blurRadius: 4,
+                          )
+                        ],
+                      ),
+                    );
+                  },
+                  scrollDirection: Axis.horizontal,
+                ),
+              ),
               SizedBox(
                 height: MediaQuery.of(context).size.height * 0.4,
                 child: PageView.builder(
                   controller: _pageController,
+                  itemCount: 10,
                   itemBuilder: ((context, index) {
                     return Container(
                       margin: const EdgeInsets.symmetric(
-                        vertical: 32,
+                        vertical: 16,
                         horizontal: 16,
                       ),
                       decoration: BoxDecoration(
                         color: Colors.white,
                         borderRadius: BorderRadius.circular(16),
-                        boxShadow: [BoxShadow(
-                          color: Colors.black.withOpacity(0.3),
-                          offset: const Offset(0, 6),
-                          blurRadius: 8,)],
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.3),
+                            offset: const Offset(0, 6),
+                            blurRadius: 8,
+                          )
+                        ],
                       ),
-                      child: Center(child: Text('Page $index'),),
+                      child: Center(
+                        child: Text('Page $index'),
+                      ),
                     );
                   }),
                 ),
               ),
+              SizedBox(height: 20,),
+              SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.3,
+                  width: MediaQuery.of(context).size.width * 0.85,
+                  child: Column(
+                    children: [
+                      Text(
+                        'Statistics:',
+                        textAlign: TextAlign.start,
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold,),
+                      ),
+                    ],
+                  )),
             ],
           ),
         ),
