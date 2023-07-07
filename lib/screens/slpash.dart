9fd131c29eea26d7642b79c59d8a920862d5866e
Lorenzo Sterzi <lorenzo.sterzi@studenti.unipd.it>
2023-07-07 16:04:20 +0200

    splash page, authentication and tokens check

--- lib/screens/slpash.dart
+++ lib/screens/slpash.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
-//import 'package:app_project/screens/home_page.dart';
+import 'package:provider/provider.dart';
 
+import 'package:app_project/services/impact.dart';
+import 'package:app_project/utils/shared_preferences.dart';
+
+import 'package:app_project/screens/home_page.dart';
+import 'package:app_project/screens/login/login_page.dart';
+import 'package:app_project/screens/onboarding/impact_ob.dart';
+
+// MUST MODIFY THE UI (ln. 68 to end), THE OTHER LINES ARE OK
 
 class Splash extends StatelessWidget {
   static const route = '/splash/';
@@ -8,16 +16,16 @@ class Splash extends StatelessWidget {
 
   const Splash({Key? key}) : super(key: key);
 
-  /*// Method for navigation SplashPage -> LoginPage
+  // Method for navigation SplashPage -> LoginPage
   void _toLoginPage(BuildContext context) {
-    Navigator.of(context)
-        .pushReplacement(MaterialPageRoute(builder: ((context) => Login())));
+    Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: ((context) => LoginPage())));
   } //_toLoginPage
 
   // Method for navigation SplashPage -> HomePage
   void _toHomePage(BuildContext context) {
     Navigator.of(context)
-        .pushReplacement(MaterialPageRoute(builder: ((context) => Home())));
+        .pushReplacement(MaterialPageRoute(builder: ((context) => HomePage())));
   } //_toHomePage
 
   // Method for navigation SplashPage -> Impact
@@ -26,29 +34,64 @@ class Splash extends StatelessWidget {
         MaterialPageRoute(builder: ((context) => ImpactOnboarding())));
   }
 
+
+  // Token and authentication check function
   void _checkAuth(BuildContext context) async {
     var prefs = Provider.of<Preferences>(context, listen: false);
     String? username = prefs.username;
     String? password = prefs.password;
 
-  }*/
+    // username and password were defined inside the shared preferences
 
+    // no user logged in the app (authentication check)
+    if (username == null || password == null) {
+      Future.delayed(const Duration(seconds: 1), () => _toLoginPage(context));
+    } else {
+
+      // Tokens check
+      ImpactService service =
+          Provider.of<ImpactService>(context, listen: false);
+      bool responseAccessToken = service.checkSavedToken();
+      bool refreshAccessToken = service.checkSavedToken(refresh: true);
+
+      // if we have a valid token for impact, proceed
+      if (responseAccessToken || refreshAccessToken) {
+        Future.delayed(const Duration(seconds: 1), () => _toHomePage(context));
+      } else {
+        Future.delayed(
+            const Duration(seconds: 1), () => _toImpactPage(context));
+      }
+    }
+  }
+
+
+  // User Interface (TO BE REARRANGED)
   @override
   Widget build(BuildContext context) {
     //Future.delayed(const Duration(seconds: 5), () => _toLoginPage(context));
-    //Future.delayed(const Duration(seconds: 1), () => _checkAuth(context));
+    Future.delayed(const Duration(seconds: 1), () => _checkAuth(context));
     return Material(
       child: Container(
-        //color: Color.fromARGB(255, 251, 251, 228),
-        alignment: Alignment.center,
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/gif/Fit2Seed.gif"), 
-              fit: BoxFit.cover),
-          ),
-          )
-
-      ,)                 
-   ;
+        color: const Color(0xFF83AA99),
+        child: const Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Text(
+              'Fit2Seed',
+              style: TextStyle(
+                  color: Color(0xFFE4DFD4),
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold),
+            ),
+            Center(
+              child: CircularProgressIndicator(
+                strokeWidth: 4,
+                valueColor: AlwaysStoppedAnimation<Color>(Color(0xFF89453C)),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
